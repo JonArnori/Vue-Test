44ca7c2131b0a652cb73b47d73d3e3b0fb3db978
JonArnori <94056694+JonArnori@users.noreply.github.com>
2024-02-27 19:33:24 +0100

    Add files via upload

---
+++ vue-jest-test-master/README.md
@@ -0,0 +1,27 @@
+# vue-jest
+
+> Use the jest test framework to unit and integrate test cases for the vuejs project.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](https://github.com/lenvonsam/vue-jest-test/wiki/*-Vue-jest-test-%E5%BC%95%E8%A8%80) and [docs for test report](https://juejin.im/post/5affc8556fb9a07aa6320833).
---
+++ vue-jest-test-master/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+    </ul>
+    <button id="testBtn" @click="clickCb('这是测试按钮')">测试按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    clickCb: {
+      type: Function
+    }
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
